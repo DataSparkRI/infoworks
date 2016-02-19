e03df46c5fb7ce529bb9f4c9a717f7529f080440
yangxu <xuyang85@yahoo.com>
2016-02-19 14:44:50 +0000

    update model

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from data.models import IndicatorTitle, District, DistrictIndicatorSet, DistrictIndicator, School, SchoolIndicatorSet, SchoolIndicator, SchoolNumberOfStudentAndTeacher, DistrictNumberOfStudentAndTeacher
+from data.models import DistrictIndicatorDataSet, SchoolIndicatorDataSet, IndicatorTitle, District, DistrictIndicatorSet, DistrictIndicator, School, SchoolIndicatorSet, SchoolIndicator, SchoolNumberOfStudentAndTeacher, DistrictNumberOfStudentAndTeacher
 
 
 admin.site.register(IndicatorTitle)
@@ -12,23 +12,28 @@ class DistrictNumberOfStudentAndTeacherInline(admin.TabularInline):
 class DistrictIndicatorSetInline(admin.TabularInline):
     model = DistrictIndicatorSet
 
+class DistrictIndicatorDataSetInline(admin.TabularInline):
+    model = DistrictIndicatorDataSet
+
 class DistrictAdmin(admin.ModelAdmin):
-    list_display = ('district_name', 'activate','slug', 'modified')
+    list_display = ('district_name', 'activate','slug', 'indicator_modified')
     inlines = [DistrictNumberOfStudentAndTeacherInline, DistrictIndicatorSetInline]
 admin.site.register(District, DistrictAdmin)
     
 class DistrictIndicatorSetAdmin(admin.ModelAdmin):
     list_display = ('district', 'title')
+    search_fields = ['district__district_name','title']
 admin.site.register(DistrictIndicatorSet, DistrictIndicatorSetAdmin)
 
 class DistrictIndicatorAdmin(admin.ModelAdmin):
-    list_display = ('district_indicator_set','title','school_year','created','modified')
+    list_display = ('district_indicator_set','title','created','modified')
+    raw_id_fields = ('district_indicator_set',)
+    inlines = [DistrictIndicatorDataSetInline]
 admin.site.register(DistrictIndicator, DistrictIndicatorAdmin)
 
-class DistrictNumberOfStudentAndTeacherAdmin(admin.ModelAdmin):
-    list_display = ('district', 'school_year','student', 'teacher', 'school')
-admin.site.register(DistrictNumberOfStudentAndTeacher, DistrictNumberOfStudentAndTeacherAdmin)
-
+#class DistrictNumberOfStudentAndTeacherAdmin(admin.ModelAdmin):
+#    list_display = ('district', 'school_year','student', 'teacher', 'school')
+#admin.site.register(DistrictNumberOfStudentAndTeacher, DistrictNumberOfStudentAndTeacherAdmin)
 
 
 class SchoolNumberOfStudentAndTeacherInline(admin.TabularInline):
@@ -37,21 +42,36 @@ class SchoolNumberOfStudentAndTeacherInline(admin.TabularInline):
 class SchoolIndicatorSetInline(admin.TabularInline):
     model = SchoolIndicatorSet
 
+
+class SchoolNumberOfStudentAndTeacherInline(admin.TabularInline):
+    model = SchoolNumberOfStudentAndTeacher
+
+class SchoolIndicatorSetInline(admin.TabularInline):
+    model = SchoolIndicatorSet
+
+class SchoolIndicatorDataSetInline(admin.TabularInline):
+    model = SchoolIndicatorDataSet
+
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('school_name', 'activate','slug', 'modified')
+    list_display = ('school_name', 'activate','slug', 'indicator_modified','grade_pk','grade_k','grade_1','grade_2','grade_3','grade_4','grade_5','grade_6','grade_7','grade_8','grade_9','grade_10','grade_11','grade_12')
+    list_editable = ('grade_pk','grade_k','grade_1','grade_2','grade_3','grade_4','grade_5','grade_6','grade_7','grade_8','grade_9','grade_10','grade_11','grade_12')
+    inlines = [SchoolNumberOfStudentAndTeacherInline, SchoolIndicatorSetInline]
 admin.site.register(School, SchoolAdmin)
 
 class SchoolIndicatorSetAdmin(admin.ModelAdmin):
     list_display = ('school', 'title')
+    search_fields = ['school__school_name','title']
 admin.site.register(SchoolIndicatorSet, SchoolIndicatorSetAdmin)
 
 class SchoolIndicatorAdmin(admin.ModelAdmin):
-    list_display = ('school_indicator_set','title','school_year','created','modified')
+    list_display = ('school_indicator_set','title','created','modified')
+    raw_id_fields = ('school_indicator_set',)
+    inlines = [SchoolIndicatorDataSetInline]
 admin.site.register(SchoolIndicator, SchoolIndicatorAdmin)
 
-class SchoolNumberOfStudentAndTeacherAdmin(admin.ModelAdmin):
-    list_display = ('school', 'school_year','student', 'teacher')
-admin.site.register(SchoolNumberOfStudentAndTeacher, SchoolNumberOfStudentAndTeacherAdmin)
+#class SchoolNumberOfStudentAndTeacherAdmin(admin.ModelAdmin):
+#    list_display = ('school', 'school_year','student', 'teacher')
+#admin.site.register(SchoolNumberOfStudentAndTeacher, SchoolNumberOfStudentAndTeacherAdmin)
 
 
 
--- data/models.py
+++ data/models.py
@@ -18,6 +18,7 @@ class IndicatorTitle(models.Model):
 
 class District(models.Model):
     district_name = models.CharField(max_length=100)
+    district_code = models.CharField(max_length=100,unique=True)
     activate = models.BooleanField(default=True)
     website = models.URLField(blank=True)
     slug = models.SlugField(unique=True,db_index=True)
@@ -28,14 +29,15 @@ class District(models.Model):
     zip = models.CharField(max_length=100, blank=True)
     phone = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
-    modified    = models.DateTimeField(blank=True)
+    indicator_modified    = models.DateTimeField(blank=True, null=True)
     
     def __unicode__(self):
         return "%s District"% self.district_name
 
 class DistrictNumberOfStudentAndTeacher(models.Model):
     district = models.ForeignKey(District)
-    school_year = models.CharField(max_length=100,blank=False,unique=True)
+    activate = models.BooleanField(default=False)
+    school_year = models.CharField(max_length=100,blank=False)
     student = models.IntegerField(null=True)
     teacher = models.IntegerField(null=True)
     school = models.IntegerField(null=True)
@@ -45,10 +47,11 @@ class DistrictNumberOfStudentAndTeacher(models.Model):
 
 class DistrictIndicatorSet(models.Model):
     district = models.ForeignKey(District)
-    title = models.CharField(max_length=100,blank=False,unique=True)
+    title = models.CharField(max_length=100,blank=False)
+    order = models.IntegerField(default=1)
 
     def __unicode__(self):
-        return "%s %s"% (self.district.district_name, self.title)
+        return "%s - %s"% (self.district.district_name, self.title)
         
 class DistrictIndicator(models.Model):
     district_indicator_set = models.ForeignKey(DistrictIndicatorSet, blank=True, null=True)
@@ -56,23 +59,32 @@ class DistrictIndicator(models.Model):
     short_title = models.CharField(max_length=100,blank=True)
     description = models.TextField(blank=True)
     data_indeicator = models.BooleanField(default=True)
-    school_year = models.CharField(max_length=100,blank=False)
     created     = models.DateTimeField(editable=False)
-    modified    = models.DateTimeField()
+    modified    = models.DateTimeField(blank=True)
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
-        self.district_indicator_set.district.modified = timezone.now()
+        self.district_indicator_set.district.indicator_modified = timezone.now()
+        self.district_indicator_set.district.save()
         return super(DistrictIndicator, self).save(*args, **kwargs)
         
     def __unicode__(self):
         return "%s - %s"% (self.district_indicator_set, self.title)
 
-class DistrictIndicatorData(models.Model):
+class DistrictIndicatorDataSet(models.Model):
     district_indicator = models.ForeignKey(DistrictIndicator, blank=True, null=True)
+    shcool_year = models.CharField(max_length=100,blank=False)
+    csv_file = models.FileField(upload_to="District_Indicator_Data", blank=True, null=True)
+    import_file = models.BooleanField(default=False) #If True start import file, then mark False after
+    
+    def __unicode__(self):
+        return "%s - %s"%(self.district_indicator, self.school_year)
+
+class DistrictIndicatorData(models.Model):
+    district_indicator_dataset = models.ForeignKey(DistrictIndicatorDataSet, blank=True, null=True)
     dimension_x = models.CharField(max_length=100, blank=True)
     dimension_y = models.CharField(max_length=100, blank=True)
     key_value = models.CharField(max_length=100, db_index=True)
@@ -84,7 +96,9 @@ class DistrictIndicatorData(models.Model):
 
 class School(models.Model):
     district = models.ForeignKey(District, blank=True, null=True)
+    school_code = models.CharField(max_length=100,unique=True)
     school_name = models.CharField(max_length=100)
+    school_type = models.CharField(max_length=100)
     activate = models.BooleanField(default=True)
     website = models.URLField(blank=True)
     slug = models.SlugField(unique=True,db_index=True)
@@ -109,14 +123,15 @@ class School(models.Model):
     grade_10 = models.BooleanField(default=False)
     grade_11 = models.BooleanField(default=False)
     grade_12 = models.BooleanField(default=False)
-    modified    = models.DateTimeField(blank=True)
+    indicator_modified    = models.DateTimeField(blank=True, null=True)
     
     def __unicode__(self):
         return "%s School"% self.school_name
         
 class SchoolNumberOfStudentAndTeacher(models.Model):
     school = models.ForeignKey(School)
-    school_year = models.CharField(max_length=100,blank=False,unique=True)
+    activate = models.BooleanField(default=False)
+    school_year = models.CharField(max_length=100,blank=False)
     student = models.IntegerField(null=True)
     teacher = models.IntegerField(null=True)
 
@@ -126,10 +141,11 @@ class SchoolNumberOfStudentAndTeacher(models.Model):
 
 class SchoolIndicatorSet(models.Model):
     school = models.ForeignKey(School, blank=True, null=True)
-    title = models.CharField(max_length=100,blank=False,unique=True)
+    title = models.CharField(max_length=100,blank=False)
+    order = models.IntegerField(default=0)
 
     def __unicode__(self):
-        return "%s %s"% (self.school.school_name, self.title)
+        return "%s - %s"% (self.school.school_name, self.title)
         
 class SchoolIndicator(models.Model):
     school_indicator_set = models.ForeignKey(SchoolIndicatorSet, blank=True, null=True)
@@ -137,23 +153,32 @@ class SchoolIndicator(models.Model):
     short_title = models.CharField(max_length=100,blank=True)
     description = models.TextField(blank=True)
     data_indeicator = models.BooleanField(default=True)
-    school_year = models.CharField(max_length=100,blank=False)
     created     = models.DateTimeField(editable=False)
-    modified    = models.DateTimeField()
+    modified    = models.DateTimeField(blank=True)
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
             self.created = timezone.now()
         self.modified = timezone.now()
-        self.school_indicator_set.school.modified = timezone.now()
+        self.school_indicator_set.school.indicator_modified = timezone.now()
+        self.school_indicator_set.school.save()
         return super(SchoolIndicator, self).save(*args, **kwargs)
     
     def __unicode__(self):
         return "%s - %s - %s"% (self.school_indicator_set, self.title, self.school_year)
 
-class SchoolIndicatorData(models.Model):
+class SchoolIndicatorDataSet(models.Model):
     school_indicator = models.ForeignKey(SchoolIndicator, blank=True, null=True)
+    shcool_year = models.CharField(max_length=100,blank=False)
+    csv_file = models.FileField(upload_to="School_Indicator_Data", blank=True, null=True)
+    import_file = models.BooleanField(default=False) #If True start import file, then mark False after
+    
+    def __unicode__(self):
+        return "%s - %s"%(self.school_indicator, self.school_year)
+
+class SchoolIndicatorData(models.Model):
+    school_indicator_dataset = models.ForeignKey(SchoolIndicatorDataSet, blank=True, null=True)
     dimension_x = models.CharField(max_length=100, blank=True)
     dimension_y = models.CharField(max_length=100, blank=True)
     key_value = models.CharField(max_length=100, db_index=True)
