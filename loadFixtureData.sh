efb6eb4d5702e9d2db9e06a70170fd356510b4bf
Yang <yxu@provplan.org>
2016-04-11 09:10:53 -0400

    update fixtures

--- loadFixtureData.sh
+++ loadFixtureData.sh
@@ -4,3 +4,4 @@ python manage.py loaddata fixture infoworks/fixtures/initial_systemcode.json
 python manage.py loaddata fixture infoworks/fixtures/initial_lookuptable.json 
 python manage.py loaddata fixture infoworks/fixtures/initial_lookuptableelement.json
 python manage.py loaddata fixture infoworks/fixtures/initial_dimensionname.json
+python manage.py loaddata fixture infoworks/fixtures/initial_dimensionfor.json
